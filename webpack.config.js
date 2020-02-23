7e1e4e4fea468ed2df217c4b8cde038203f63c65
James Dinnes <jamesdinnes@Jamess-MacBook-Pro.local>
2020-02-24 00:28:58 +1000

    added css modules to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-const devMode = process.argv.some(arg => arg.includes('development'));
+const isDevelopment = process.argv.some(arg => arg.includes('development'));
 
 module.exports = {
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.[hash:12].js',
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/'
+    publicPath: '/',
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
   module: {
     rules: [
@@ -28,48 +28,66 @@ module.exports = {
           emitWarning: true,
           typeCheck: false,
           configFile: './tslint.json',
-          tsConfigFile: './tsconfig.json'
-        }
+          tsConfigFile: './tsconfig.json',
+        },
       },
       {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             options: {
-              importLoaders: 2
-            }
+              modules: true,
+              sourceMap: isDevelopment,
+            },
           },
-          'sass-loader'
-        ]
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
       },
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
-    ]
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+    ],
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve('./index.html'),
+        filename: 'index.html',
+        inject: 'body',
+        minify: false,
+        chunksSortMode: 'auto',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      }),
+      new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 8000,
+        proxy: 'http://localhost:8080/',
+      }),
+    ],
   },
-  externals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve('./index.html'),
-      filename: 'index.html',
-      inject: 'body',
-      minify: false,
-      chunksSortMode: 'auto'
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    }),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 8000,
-      proxy: 'http://localhost:8080/'
-    })
-  ]
 };
